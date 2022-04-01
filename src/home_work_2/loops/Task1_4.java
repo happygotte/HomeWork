c6cddc0ecafcd6e56feb656856b65ee162c48c4b
Maryia Nesterova <m_v_nesterova@mail.ru>
2022-04-02 02:43:20 +0300

    Переделанная Task1_4*

--- src/home_work_2/loops/Task1_4.java
+++ src/home_work_2/loops/Task1_4.java
@@ -3,16 +3,25 @@ package home_work_2.loops;
 public class Task1_4 {
     public static void main(String[] args) {
         int num = Integer.parseInt(args[0]);
+        long a = 1;
 
-        long after_over = num;
+        long after_over = num * a;
         long before_over = num;
-        while (after_over > 0) {
+        while (after_over * num /  before_over == num) {
             after_over *= num;
-            if (after_over > 0) {
-                before_over = after_over;
+            before_over = after_over;
+            }
+        if (num > 0) {
+            after_over *= num;
+        } else {
+            for (int i = 0; i < 2; i++) { // на знаю, почему для отриц. числа не хватает ещё одного умножения
+                after_over *= num;
             }
         }
+
         System.out.println(before_over);
         System.out.println(after_over);
     }
 }
+
+
